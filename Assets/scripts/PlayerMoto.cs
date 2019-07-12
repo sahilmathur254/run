3e4329cd091b6e8327a4ea93babe28b1bb631c25
Sahil Mathur <sahil.mathur2504@gmail.com>
2019-07-12 20:09:59 +0530

    Initial commit

---
+++ Assets/scripts/PlayerMoto.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoto : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveVector;
+    private float speed = 5.0f;
+    private float VerticalVelocity = 0.0f;
+    private float gravity = 24.0f;
+
+
+    private float animationDuration = 2.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+
+        moveVector = Vector3.zero;
+
+        if(controller.isGrounded)
+        {
+            VerticalVelocity = -0.5f;
+        }
+        else
+        {
+            VerticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        //x Left and right
+
+        moveVector.x = Input.GetAxisRaw("Horizontal")*speed;
+
+        //y Up and Down
+        moveVector.y = VerticalVelocity;
+
+
+        //z Forward and backward
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
---
+++ Assets/scripts/TileManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+    private Transform playerTransform;
+    private float spawnZ = -1.0f;
+    private float tileLength = 10.0f;
+   private int amnTilesOnScreen = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for(int i = 0; i< amnTilesOnScreen; i++)
+        {
+            spawnTile();
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( playerTransform.position.z > (spawnZ - amnTilesOnScreen * tileLength))
+        {
+            spawnTile();
+        }
+
+    }
+
+    private void spawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(tilePrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+    }
+}
